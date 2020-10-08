259604aa476fff3d7e569554b076ec32216e1988
Kasparidi <kasparidi@gmail.com>
2020-10-08 09:40:27 +0300

    added Jacoco and initial tests

--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -7,6 +7,20 @@ import static org.junit.jupiter.api.Assertions.*;
 class RadioTest {
 
     @Test
-    void getSoundVolume() {
+    void shouldReturnSoundsVolumeMore() {
+        Radio radio = new Radio();
+        int soundVolume = 11;
+        int expected = 0;
+        radio.setSoundVolume(soundVolume);
+        assertEquals(expected, radio.getSoundVolume());
+    }
+
+    @Test
+    void shouldReturnSoundsVolumeLess() {
+        Radio radio = new Radio();
+        int soundVolume = -1;
+        int expected = 0;
+        radio.setSoundVolume(soundVolume);
+        assertEquals(expected, radio.getSoundVolume());
     }
 }
